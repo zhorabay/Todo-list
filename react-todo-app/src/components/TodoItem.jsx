91f1443617e9f6b45bd6c67fd56f60d46589ba40
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-09-18 13:27:01 +0300

    Followed tutorials

---
+++ react-todo-app/src/components/TodoItem.jsx
@@ -0,0 +1,31 @@
+const TodoItem = ({ itemProp, handleChange, delTodo }) => {
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
+  return (
+    <li>
+      <input type="checkbox" checked={itemProp.completed} onChange={() => handleChange(itemProp.id)}/>
+      <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+      {itemProp.title}
+    </li>
+  );
+};
+
+export default TodoItem;
